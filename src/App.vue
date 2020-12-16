3e077332620d6fa2ad17dcceaa8e3315ba3af2c8
LyndonSNC <lyndon-nc@outlook.com>
2020-12-16 06:33:21 -0500

    iss4 - Informacion de cada integrante dle grupo.

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="app">
+
+      <image-text> </image-text>
+      <carousel> </carousel>
+      <services> </services>
+      
+      <!-- news -->
+      <div class="container-fluid">
+        <div class="row mt-md-5 mt-sm-5 mt-xs-5">
+       <news> </news>
+        </div>
+      </div>
+      
+      <!-- members -->
+      <div class="container-fluid">
+        <div class="row justify-content-center mb-5">
+          <div class="col mt-5" v-for="(miembro, index) of Team" :key="index" >
+            <team-card :member="miembro" > </team-card>
+          </div>
+        </div>
+      </div>
+
+      <footer-section> </footer-section>
+
+  </div>
+</template>
+
+<script>
+
+import ImageText from './components/ImageText.vue'
+import Carousel from './components/Carousel.vue'
+import Services from './components/Services.vue'
+import FooterSection from './components/FooterSection.vue'
+import TeamCard from './components/TeamCard.vue'
+import News from './components/News.vue'
+
+
+export default {
+  name: 'App',
+  components:{
+    ImageText,
+    Carousel,
+    Services,
+    FooterSection,
+    TeamCard,
+    News,
+  },
+
+    data() {
+      return {
+        Team:[
+          {
+          edad : 24 , 
+          nombre : 'Tatiana Castillo' ,
+          descripcion : 'Tecnólogo Costos y Auditoria, amante de los animales, sin experiencia en programación' ,
+          rol : 'Full Stack Development' , 
+          image : 'https://i.imgur.com/hhWYp55.jpeg'
+        },
+        {
+          edad : 28 , 
+          nombre : 'Juan David Cuevas Perdomo' ,
+          descripcion : 'Ingeniero químico, me gusta viajar en mi motocicleta y los videojuego' ,
+          rol : 'Full Stack Development' , 
+          image : 'https://i.imgur.com/Xpuq2GT.jpeg'
+        },
+        {
+          edad : 23 , 
+          nombre : 'David Carvalho Saldarriaga' ,
+          descripcion : 'Ingeniero Aeronautico, amante de la lectura y de un buen cafe' ,
+          rol : 'Full Stack Development' , 
+          image : 'https://i.imgur.com/xR2xRF4.jpeg'
+        },
+        {
+          edad : 25 , 
+          nombre : 'Lyndon Santiago Nuñez Caviche' ,
+          descripcion : 'Amante de los videojuegos y la musica, entusiasta en todo lo referente a tecnologia y sistemas.' ,
+          rol : 'Full Stack Development' , 
+          image : 'https://i.imgur.com/kz7Qi4n.jpg'
+        },
+        {
+          edad : 'None' , 
+          nombre : '' ,
+          descripcion : '' , 
+          image : 'https://claroplay.com/images/carousel/noimage_poster.png?_v=1.5.99'
+        },
+      ]
+
+    }
+
+  }
+
+}
+</script>
+
---
+++ src/components/TeamCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+                <div class="card text-white bg-dark">
+                    <div class="d-flex justify-content-center p-2">
+                        
+                        <img :src="member.image" alt="foto del equipo"> 
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">{{member.nombre}}</h5>
+                        <p class="card-text">{{member.descripcion}}</p>
+                        <small class="card-cod">{{member.rol}}</small>
+                    </div>
+                    <div class="card-footer bg-white">
+                        <small class="text-muted"></small>
+                    </div>
+                </div>
+            </div>
+    
+</template>
+
+<script>
+    export default {
+        name: "TeamCard",
+        props: ['member']
+    }
+</script>
+
+<style scoped>
+
+</style>
